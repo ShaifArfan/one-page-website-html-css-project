f9e3aaa0777de5b19c9d0dcbf92a781229b85b64
Moaad Afellah <moaadafellah@gmail.com>
2023-06-05 22:38:46 +0100

    add button to scroll up

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+
+
+
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
@@ -23,3 +26,22 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
+window.addEventListener("DOMContentLoaded",
+	function () {
+		let buttonOfuP = document.getElementById('buttonOfuP')
+		document.addEventListener('scroll', () => {
+			if (window.scrollY == 0)
+				buttonOfuP.classList.add("hideButtonOfuP")
+			else
+				buttonOfuP.classList.remove("hideButtonOfuP")
+		})
+		buttonOfuP.addEventListener('click', () => {
+			window.scroll(0, 0)
+		})
+	});
+
+
+
+
